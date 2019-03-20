b0bc3d123e151662542176abdf479ae95aecd4dc
Francis Chen <fncis.chen@gmail.com>
2019-03-20 15:01:20 -0700

    updated postRoutes and userRouters

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -3,21 +3,21 @@ const express = require('express');
 const server = express();
 
 server.get("/api/posts", (req,res) => {
-
+    postData.get().then().catch();
 })
 
 server.get("/api/posts:id", (req,res) => {
-
+    postData.getById(req.params.id).then().catch();
 })
 
 server.post("/api/posts", (req,res) => {
-
+    postData.insert(req.body).then().catch();
 })
 
 server.put("/api/posts:id", (req,res) => {
-
+    postData.update(req.body, req.params.id).then().catch();
 })
 
 server.delete("/api/posts:id", (req,res) => {
-
+    postData.delete(req.body, req.params.id).then().catch();
 })
--- users/userRoutes.js
+++ users/userRoutes.js
@@ -1,5 +1,23 @@
 const userData = require("../data/helpers/userDb.js");
 const express = require('express');
 const server = express();
-const router = express.Router();
 
+server.get("/api/users", (req,res) => {
+    userData.get().then().catch();
+})
+
+server.get("/api/users:id", (req,res) => {
+    userData.getById(req.params.id).then().catch();
+})
+
+server.post("/api/users", (req,res) => {
+    userData.insert(req.body).then().catch();
+})
+
+server.put("/api/users:id", (req,res) => {
+    userData.update(req.body, req.params.id).then().catch();
+})
+
+server.delete("/api/users:id", (req,res) => {
+    userData.delete(req.body, req.params.id).then().catch();
+})
