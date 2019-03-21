7e7da710fbed56e2c5fbc0737a96adaa9ff80715
Francis Chen <fncis.chen@gmail.com>
2019-03-21 12:33:28 -0700

    updated post routes and did refactoring

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -4,23 +4,38 @@ const router = express.Router();
 const postData = require("../data/helpers/postDb.js");
 
 router.get("/", (req,res) => {
-    postData.get().then().catch();
+    postData.get()
+    .then((posts) => res.status(200).json(posts))
+    .catch(() => res.status(500).json({error: "Post information cannot be retrieved"}));
 })
 
 router.get("/:id", (req,res) => {
-    postData.getById(req.params.id).then().catch();
+    postData.getById(req.params.id)
+    .then((id) => res.json(200).json(id))
+    .catch(() => res.status(400).json({ message: "The post with the specified ID does not exist." }))
 })
 
 router.post("/", (req,res) => {
-    postData.insert(req.body).then().catch();
+    postData.insert(req.body)
+    .then( (post) => res.status(201).json(post))
+    .catch(
+        () => res.status(500).json({ message: "There were errors in saving your post"})
+    );
 })
 
 router.put("/:id", (req,res) => {
-    postData.update(req.body, req.params.id).then().catch();
+    postData.update(req.body, req.params.id)
+    .then( (id) => res.status(201).json(id))
+    .catch(
+        () => res.status(500).json({ message: "There were errors in updating your post"})
+    );
+
 })
 
 router.delete("/:id", (req,res) => {
-    postData.delete(req.body, req.params.id).then().catch();
+    postData.remove(req.params.id)
+    .then((id) => res.json(id))
+    .catch(() => res.status(500).json({ message: "There were errors in deleting your post"}));
 })
 
 module.exports = router;
--- users/userRoutes.js
+++ users/userRoutes.js
@@ -6,20 +6,20 @@ const userData = require("../data/helpers/userDb.js");
 router.get("/", (req,res) => {
     userData.get()
     .then((users) => res.status(200).json(users))
-    .catch(res.status(500).json({error: "User information cannot be retrieved"}));
+    .catch(() => res.status(500).json({error: "User information cannot be retrieved"}));
 })
 
 router.get("/:id", (req,res) => {
     userData.getById(req.params.id)
-    .then((id) => res.json(id))
-    .catch(res.status(400).json({ message: "The post with the specified ID does not exist." }))
+    .then((id) => res.json(200).json(id))
+    .catch(() => res.status(400).json({ message: "The user with the specified ID does not exist." }))
 })
 
 router.post("/", (req,res) => {
     userData.insert(req.body)
     .then( (user) => res.status(201).json(user))
     .catch(
-        res.status(500).json({ message: "There were errors in saving your post"})
+        () => res.status(500).json({ message: "There were errors in adding a user"})
     );
 })
 
@@ -29,14 +29,14 @@ router.put("/:id", (req,res) => {
         (id) => res.status(201).json(user)
     )
     .catch(
-        res.status(500).json({ message: "There were errors in updating your post"})
+        () => res.status(500).json({ message: "There were errors in updating your user"})
     );
 })
 
 router.delete("/:id", (req,res) => {
-    userData.delete(req.body, req.params.id)
-    .then(res.sent("The item was successfully deleted"))
-    .catch();
+    userData.remove(req.params.id)
+    .then((id) => res.json(id))
+    .catch(() => res.status(500).json({ message: "There were errors in deleting your post"}));
 })
 
 module.exports = router;
