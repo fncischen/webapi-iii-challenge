a38880ce8277203c147eac42b8f7fac4a51f5301
Francis Chen <fncis.chen@gmail.com>
2019-03-21 16:16:46 -0700

    refactored middleware for next method

--- data/middleware/middleware.js
+++ data/middleware/middleware.js
@@ -1,10 +1,10 @@
-function middleware(req, res) {
+function middleware(req, res, next) {
     if (req.body.name.charAt(0) == req.body.name.charAt(0).toUpperCase()) {
-      res.json(req);
+      next();
     } else {
       console.log("The first letter of the name must be capitalized! I am capitalizing now.");
       req.body.name = req.body.name.charAt(0).toUpperCase() + req.body.name.substring(1, req.body.name.length);
-      res.json(req.body);
+      next();
      }
   }
 
